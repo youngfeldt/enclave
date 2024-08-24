5220bbc811d70146785e3b85112ef22322af1c5b
Chuck Youngfeldt <chuck@enclaveclouds.com>
2024-08-24 07:39:10 -0400

    made vsock  tolerant of socket in use

--- vsock/src/main.rs
+++ vsock/src/main.rs
@@ -1,11 +1,13 @@
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::io::prelude::*;
 use serde_json::Value;
+use std::fs;
+use std::path::Path;
 
 fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
     let mut buffer = String::new();
     stream.read_to_string(&mut buffer)?;
-    
+
     let attestation_doc: Value = serde_json::from_str(&buffer).expect("Invalid JSON");
 
     if let Some(pcrs) = attestation_doc.get("pcrs") {
@@ -21,17 +23,32 @@ fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = UnixListener::bind("/vsock/vm.sock")?;
+    let socket_path = "/vsock/vm.sock";
 
+    // Ensure the directory exists
+    if let Some(parent) = Path::new(socket_path).parent() {
+        fs::create_dir_all(parent)?;
+    }
+
+    // Attempt to remove existing socket file
+    if Path::new(socket_path).exists() {
+        if let Err(e) = fs::remove_file(socket_path) {
+            eprintln!("Failed to remove existing socket file: {}", e);
+        }
+    }
+
+    let listener = UnixListener::bind(socket_path)?;
     println!("VSOCK listener waiting for connections...");
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_client(stream)?;
+                if let Err(err) = handle_client(stream) {
+                    eprintln!("Failed to handle client: {}", err);
+                }
             }
             Err(err) => {
-                println!("Connection failed: {}", err);
+                eprintln!("Connection failed: {}", err);
             }
         }
     }
