9ca9b832d0700f1be860ca4dbc2dbf6d637d8733
Chuck Youngfeldt <chuck@enclaveclouds.com>
2024-08-24 07:47:59 -0400

    updated enclave code to use vsock

--- enclave_attestation/src/main.rs
+++ enclave_attestation/src/main.rs
@@ -1,10 +1,26 @@
-use std::fs::File;
 use std::io::prelude::*;
-use std::os::unix::net::UnixStream;
-use std::process::Command;
+use vsock::VsockStream;
+
+fn main() -> std::io::Result<()> {
+    let cid = 3; // CID for the parent instance
+    let port = 5005; // The same port number used by the parent listener
+
+    // Connect to the parent instance VSOCK listener
+    let mut stream = VsockStream::connect(cid, port)?;
+
+    let attestation_doc = get_attestation_document("example_user_data")?;
+    stream.write_all(attestation_doc.as_bytes())?;
+
+    println!("Sent attestation document to parent. Staying alive...");
+
+    // Keep the enclave alive
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(60));
+    }
+}
 
 fn get_attestation_document(user_data: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let output = Command::new("nsm-cli")
+    let output = std::process::Command::new("nsm-cli")
         .arg("request-attestation")
         .arg("--user-data")
         .arg(user_data)
@@ -16,26 +32,4 @@ fn get_attestation_document(user_data: &str) -> Result<String, Box<dyn std::erro
 
     let attestation_doc = String::from_utf8(output.stdout)?;
     Ok(attestation_doc)
-}
-
-fn send_to_vsock_server(attestation_doc: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let vsock_path = "/vsock/vm.sock"; // Path to the VSOCK socket file
-    let mut stream = UnixStream::connect(vsock_path)?;
-
-    stream.write_all(attestation_doc.as_bytes())?;
-    Ok(())
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let user_data = "example_user_data";
-    let attestation_doc = get_attestation_document(user_data)?;
-
-    send_to_vsock_server(&attestation_doc)?;
-
-    println!("Sent attestation document. Waiting to stay alive...");
-
-    // Keep the enclave alive by sleeping indefinitely
-    loop {
-        std::thread::sleep(std::time::Duration::from_secs(60));
-    }
 }
