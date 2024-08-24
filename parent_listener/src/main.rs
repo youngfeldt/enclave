50b78a8b77491e934474e5798aaed1467d8912a1
Chuck Youngfeldt <chuck@enclaveclouds.com>
2024-08-24 07:44:10 -0400

    corrected parent code to use vsock lib

--- parent_listener/src/main.rs
+++ parent_listener/src/main.rs
@@ -1,11 +1,11 @@
-use std::os::unix::net::{UnixListener, UnixStream};
 use std::io::prelude::*;
 use serde_json::Value;
+use vsock::VsockListener;
 
-fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
+fn handle_client(mut stream: vsock::VsockStream) -> std::io::Result<()> {
     let mut buffer = String::new();
     stream.read_to_string(&mut buffer)?;
-    
+
     let attestation_doc: Value = serde_json::from_str(&buffer).expect("Invalid JSON");
 
     if let Some(pcrs) = attestation_doc.get("pcrs") {
@@ -21,21 +21,25 @@ fn handle_client(mut stream: UnixStream) -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = UnixListener::bind("/vsock/vm.sock")?;
+    // Define the VSOCK port on which the parent instance will listen for connections
+    let vsock_port = 5005; // Use the same port number that the enclave will connect to
 
-    println!("VSOCK listener waiting for connections...");
+    // Create a VSOCK listener
+    let listener = VsockListener::bind(vsock_port)?;
+    println!("VSOCK listener waiting for connections on port {}...", vsock_port);
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_client(stream)?;
+                if let Err(err) = handle_client(stream) {
+                    eprintln!("Failed to handle client: {}", err);
+                }
             }
             Err(err) => {
-                println!("Connection failed: {}", err);
+                eprintln!("Connection failed: {}", err);
             }
         }
     }
 
     Ok(())
 }
-
