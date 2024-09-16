715053fb859c025c8a105554089c5c47f0442a51
Chuck Youngfeldt <chuck@enclaveclouds.com>
2024-09-16 09:04:26 -0400

    updates to readnme

--- README.md
+++ README.md
@@ -8,5 +8,6 @@
 ## To Make
 
 1. cd enclave attestation
-2. docker build enclave_attestation -t . 
+2. docker build -t enclave_attestation . 
 3. nitro-cli build-enclave --docker-uri enclave_attestation --output-file enclave_attestation.eif
+4. nitro-cli run-enclave --eif-path enclave_attestation.eif --cpu-count 2 --memory 256 --enclave-cid 10
--- vsock/target/debug/deps/vsock-e5b134efe5bd6dbd.d
+++ vsock/target/debug/deps/vsock-e5b134efe5bd6dbd.d
@@ -1,5 +1,5 @@
-/Users/fierce/workspace/enclave/vsock/target/debug/deps/libvsock-e5b134efe5bd6dbd.rmeta: /Users/fierce/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vsock-0.5.0/src/lib.rs
+/Users/fierce/workspace/enclave/enclave/vsock/target/debug/deps/libvsock-e5b134efe5bd6dbd.rmeta: /Users/fierce/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vsock-0.5.0/src/lib.rs
 
-/Users/fierce/workspace/enclave/vsock/target/debug/deps/vsock-e5b134efe5bd6dbd.d: /Users/fierce/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vsock-0.5.0/src/lib.rs
+/Users/fierce/workspace/enclave/enclave/vsock/target/debug/deps/vsock-e5b134efe5bd6dbd.d: /Users/fierce/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vsock-0.5.0/src/lib.rs
 
 /Users/fierce/.cargo/registry/src/index.crates.io-6f17d22bba15001f/vsock-0.5.0/src/lib.rs:
